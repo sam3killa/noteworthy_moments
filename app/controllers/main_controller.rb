b992020352f572906e0da7038752ce1d86a38e81
Samuel Shih <samuels.shih@gmail.com>
2013-11-13 10:11:59 -0800

    Initial import of my app

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,20 @@
+class MainController < ApplicationController
+
+	def index
+
+	end
+
+	def about
+
+	end
+
+	def how_it_works
+
+	end
+
+	def moments
+
+	end
+
+
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SongMoments::Application.config.session_store :cookie_store, key: '_song_moments_session'
