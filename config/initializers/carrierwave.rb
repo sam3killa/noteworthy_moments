bcbc2b752002e7e6e6f3c917a93de985cb4dc165
Samuel Shih <samuels.shih@gmail.com>
2014-01-06 10:22:12 -0800

    changed keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',               
-    :aws_access_key_id      => 'AKIAJ2VDZE4L24TLHL4Q',
-    :aws_secret_access_key  => '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'
+    :aws_access_key_id      => 'access key',
+    :aws_secret_access_key  => 'secret key'
   }
   if Rails.env.development?
     config.fog_directory = 'stuff_finder_samuel_shih_dev'
